b7b09b7a757e3475d948b8ce0f5a628847c90122
VazquezNicolas <nicolasorlandovazquez@gmail.com>
2023-05-31 22:58:30 -0300

    primer comentario

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo')
+const handlebars = require('express-handlebars')
+const router = require('./router')
+const mongoConnect = require('../db')
+
+const app = express()
+const port = 3000
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(__dirname + '/public'))
+app.use(
+  session({
+    store: MongoStore.create({
+        mongoUrl:
+        'mongodb+srv://nicolasorlandovazquez:Nicrecor525@cluster0.tikbzrp.mongodb.net/51120sesions?retryWrites=true&w=majority',
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+    }),
+    secret: 'coderSecret',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+app.engine('handlebars', handlebars.engine())
+app.set('view engine', 'handlebars')
+app.set('views', __dirname + '/views')
+
+mongoConnect()
+
+router(app)
+
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`)
+})
